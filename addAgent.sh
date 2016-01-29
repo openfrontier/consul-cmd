de2070d71390ff28241dab27d16b9d28b9161cdd
thinkernel <thinkernel@gmail.com>
2016-01-29 17:00:29 +0800

    Bind client listening to 0.0.0.0

--- addAgent.sh
+++ addAgent.sh
@@ -5,7 +5,7 @@ BIND_IP=$1
 JOIN_IP=$2
 consul agent \
     -bind=${BIND_IP} \
-    -client=${BIND_IP} \
+    -client=0.0.0.0 \
     -data-dir="/tmp/consul" \
     -retry-join=${JOIN_IP} \
     -ui
--- startCluster.sh
+++ startCluster.sh
@@ -5,7 +5,7 @@ BIND_IP=$1
 consul agent \
     -bind=${BIND_IP} \
     -bootstrap-expect=3 \
-    -client=${BIND_IP} \
+    -client=0.0.0.0 \
     -data-dir="/tmp/consul" \
     -server \
     -ui
